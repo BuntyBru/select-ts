00c55405a005210cd679fa5b8639695e17ae5530
BuntyBru <buntybru@gmail.com>
2022-11-22 00:26:55 +0530

    deleting all that

--- src/Select.tsx
+++ src/Select.tsx
@@ -15,28 +15,36 @@ type SelectProps = {
 const Select = ({ value, onChange, options }: SelectProps) => {
   const [isOpen, setIsOpen] = React.useState(false);
   return (
-    <>
-      <div
-        contentEditable={true}
-        onBlur={() => {
-          setIsOpen(false);
-        }}
-        onClick={() => {
-          setIsOpen(prev => !prev);
-        }}
-        className={styles.container}>
-        <span className={styles.value}>{value?.label}</span>
-        <button className={styles['clear-btn']}>&times;</button>
-        <div className={styles.caret}></div>
-        <ul className={`${styles.options} ${isOpen ? styles.show : ''}`}>
-          {options.map(option => (
-            <li key={option.label} className={styles.option}>
-              {option.label}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <div className="select-container">
+      <input />
+      <ul>
+        <li>List Item 1</li>
+        <li>List Item 2</li>
+        <li>List Item 3</li>
+        <li>List Item 4</li>
+      </ul>
+    </div>
+
+    //   <div
+    //     contentEditable={true}
+    //     onBlur={() => {
+    //       setIsOpen(false);
+    //     }}
+    //     onClick={() => {
+    //       setIsOpen(prev => !prev);
+    //     }}
+    //     className={styles.container}>
+    //     <span className={styles.value}>{value?.label}</span>
+    //     <button className={styles['clear-btn']}>&times;</button>
+    //     <div className={styles.caret}></div>
+    //     <ul className={`${styles.options} ${isOpen ? styles.show : ''}`}>
+    //       {options.map(option => (
+    //         <li key={option.label} className={styles.option}>
+    //           {option.label}
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </div>
   );
 };
 
